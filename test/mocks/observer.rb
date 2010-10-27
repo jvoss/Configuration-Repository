ec5105c825026b8293fc27602755c70bab0f2c3b
Jonathan Voss <jvoss@onvox.net>
2010-10-27 19:41:26 -0400

    Updated test case for host. Added observable object mock to test host updates.

---
+++ test/mocks/observer.rb
@@ -0,0 +1,37 @@
+# Copyright 2010 Andrew R. Greenwood and Jonathan P. Voss
+#
+# This file is part of Configuration Repository (CR)
+#
+# Configuration Repository (CR) is free software: you can redistribute 
+# it and/or modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation, either version 3 of the 
+# License, or (at your option) any later version.
+#
+# Configuration Repository (CR) is distributed in the hope that it will 
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CR. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module CRTest
+
+  class Observer
+    
+    attr_reader :config, :hostobj
+    
+    def initialize
+      @config  = 'config here'
+      @hostobj = 'host object here'
+    end # def initialize
+    
+    def update(hostobj, config)
+      @config  = config
+      @hostobj = hostobj
+    end # def update
+    
+  end # class Observer
+  
+end # module CRTest
--- test/tc_host.rb
+++ test/tc_host.rb
@@ -17,91 +17,97 @@
 #
 
 require 'rubygems'
+require 'logger'
 require 'test/unit'
-require 'shoulda'
 require 'test/test_helpers'
 require 'cr/host'
+require 'test/mocks/host'
+require 'test/mocks/observer'
 
 module CRTest
   
   class Test_host < Test::Unit::TestCase
     
-    TEST_HOST         = 'host.domain.tld'
-    TEST_USER         = 'username'
-    TEST_PASS         = 'p@$$word'
-    TEST_SNMP_OPTIONS = CRTest::TEST_OPTIONS[:snmp_options]
+    def setup
+      @snmp_options = { :Port      => 10161,
+                        :Community => 'ppuuubllicc',
+                        :Version   => :SNMPv1,
+                        :Timeout   => 7,
+                        :Retries   => 1 }
+      
+      @test_options = { :hostname     => 'host.domain.tld',
+                        :username     => 'username',
+                        :password     => 'password',
+                        :log          => Logger.new(nil),
+                        :snmp_options => @snmp_options
+                      }
+
+      @host = CR::Host.new(@test_options)
+    end # def setup
     
-    ## helper methods ##
+    def test_comparable
+      other_host = CR::Host.new(@test_options)
+      
+      assert @host == 'host.domain.tld'
+      assert_equal @host, other_host
+    end # def test_comparable
     
-    def assert_host_attributes(host_obj, attributes)
-      
-      obj_hostname     = host_obj.instance_variable_get(:@hostname)
-      obj_username     = host_obj.instance_variable_get(:@username)
-      obj_password     = host_obj.instance_variable_get(:@password)
-      obj_snmp_options = host_obj.instance_variable_get(:@snmp_options)
-      
-      assert_equal attributes[:hostname],     obj_hostname
-      assert_equal attributes[:username],     obj_username
-      assert_equal attributes[:password],     obj_password
-      assert_equal attributes[:snmp_options], obj_snmp_options
-      
-    end # def assert_host_attributes
+    def test_config
+      # Assert nil is returned because a driver has not been loaded 
+      # and therefor, overwritten.
+      assert_nil @host.config
+    end # def test_config
     
-    ## end helper methods ##
+    def test_driver
+      assert @host.respond_to?(:driver)
+      assert_equal nil, @host.driver
+      
+      test_options = @test_options.dup
+      test_options[:driver] = 'cisco'
+      
+      host = CR::Host.new(test_options)
+      assert_equal CR::Host::Cisco, host.driver
+    end # def test_driver
     
-    # test initialize(hostname, username, password, snmp_options = {})
-    #
-    context "Initializing a host object" do
-      
-      should "return a host object with the default SNMP options" do
-        
-        exp_snmp_options = TEST_OPTIONS[:snmp_options].dup
-        exp_snmp_options[:Host] = TEST_HOST
-                             
-        exp_attributes   = { :hostname     => TEST_HOST, 
-                             :username     => TEST_USER,
-                             :password     => TEST_PASS,
-                             :snmp_options => exp_snmp_options }
-                             
-        host = CR::Host.new(TEST_HOST, TEST_USER, TEST_PASS)
-        
-        assert_host_attributes(host, exp_attributes)
-        
-      end # should "return a host object with the proper attributes"
-      
-      should "return a host with custom SNMP options if supplied" do
-      
-        snmp_options   =   { :Port      => 1200,
-                             :Community => 'coMMunity',
-                             :Version   => :SNMPv1,
-                             :Timeout   => 400,
-                             :Retries   => 50, 
-                             :Host      => TEST_HOST }
-                             
-        exp_attributes =   { :hostname     => TEST_HOST, 
-                             :username     => TEST_USER,
-                             :password     => TEST_PASS,
-                             :snmp_options => snmp_options }
-                             
-        host = CR::Host.new(TEST_HOST, TEST_USER, TEST_PASS, snmp_options)
-                             
-        assert_host_attributes(host, exp_attributes)
-        
-      end # should "return a host with custom SNMP options if supplied"
-      
-    end # context "Initalizing a host object"
+    def test_hostname
+      assert @host.respond_to?(:hostname)
+      assert_equal @test_options[:hostname], @host.hostname
+    end # def test_hostname
     
-    context "Retrieving a configuration from a host" do
-      
-      should "return nil if a driver has not been loaded" do
-        
-        host = CR::Host.new(TEST_HOST, TEST_USER, TEST_PASS)  
-        assert_nil host.config
-        
-      end # should "return nil if a driver has not been loaded"
-      
-    end # context "Retrieving a configuration from a host"
+    def test_password
+      assert @host.respond_to?(:password)
+      assert_equal @test_options[:password], @host.password
+    end # def test_password
     
+    def test_process
+      observer = Observer.new
+      
+      @host.add_observer(observer)
+      @host.process
+      
+      assert_nil   observer.config
+      assert_equal @host, observer.hostobj
+    end # def test_process
+    
+    def test_snmp_options
+      obj_snmp_options = @host.instance_variable_get(:@snmp_options)
+      
+      # Ensure SNMP options include the hostname
+      snmp_options = @snmp_options.dup
+      snmp_options[:Host] = @test_options[:hostname]
+      
+      assert_equal snmp_options, obj_snmp_options
+    end # def test_snmp_options
+    
+    def test_to_s
+      assert_equal @test_options[:hostname], @host.to_s
+    end # def test_to_s
+    
+    def test_username
+      assert @host.respond_to?(:username)
+      assert_equal @test_options[:username], @host.username
+    end # def test_username
+        
   end # class Test_host
   
 end # module CRTest
